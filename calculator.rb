82dfee454393e665846800d3a6ed919746eeb2f1
Jonathan Schack <jschack94@gmail.com>
2019-11-20 02:35:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,5 @@ first_number = 3
 second_number = 4
 sum = first_number + second_number
 difference = first_number - second_number
-product = first_number - second_number
-quotient = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
